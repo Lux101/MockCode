92a7d4fdc05793507a033451ff7354e95a43e4aa
unknown <coolluke8563@gmail.com>
2018-11-17 14:46:18 -0800

    WORK

---
+++ src/Subsystems/Arm.cpp
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Arm.h"
+#include "../RobotMap.h"
+#include "ctre/Phoenix.h"
+#include "../Commands/MoveArm.h"
+
+Arm::Arm() : Subsystem("ExampleSubsystem"),
+	ArmMotor(new TalonSRX(1))
+{
+}
+
+void Arm::moveArm(double ArmPower){
+	ArmMotor->Set(ControlMode::PercentOutput, ArmPower);
+}
+
+void Arm::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new MoveArm());
+}
+
+
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ src/Subsystems/DriveTrain.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <Subsystems/DriveTrain.h>
+#include "../RobotMap.h"
+#include "../Commands/TankDrive.h"
+
+DriveTrain::DriveTrain() : Subsystem("DriveTrain"),
+left(new TalonSRX(0)),
+right(new TalonSRX(1)){
+
+}
+
+void DriveTrain::tankDrive(double leftPower, double rightPower){
+	left->Set(ControlMode::PercentOutput, -leftPower);
+	right->Set(ControlMode::PercentOutput, rightPower);
+}
+void DriveTrain::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new TankDrive());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
